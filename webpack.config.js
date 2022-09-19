e89a57adc39cd7050515a6b7dcb7578add8837e6
ihziluoh <ihziluoh@users.noreply.github.com>
2022-09-19 15:45:39 +0800

    修改目录结构

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const isProduction = process.env.NODE_ENV == "production";
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const config = {
-  entry: "./src/index.js",
+  entry: "./src/js/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
   },
@@ -19,7 +19,7 @@ const config = {
     new HtmlWebpackPlugin({
         // 以 public/index.html 为模板创建文件
         // 新的html文件有两个特点：1. 内容和源文件一致 2. 自动引入打包生成的js等资源
-        template: path.resolve(__dirname, "./src/index.html"),
+        template: path.resolve(__dirname, "./src/html/index.html"),
     }),
 
     // Add your plugins here
