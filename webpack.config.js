baccae7c09b4516f444398064822bacda0a3caa1
ihziluoh <ihziluoh@users.noreply.github.com>
2022-09-19 15:43:06 +0800

    增加插件 html-webpack-plugin 解析html

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@
 const path = require("path");
 
 const isProduction = process.env.NODE_ENV == "production";
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const config = {
   entry: "./src/index.js",
@@ -14,6 +15,13 @@ const config = {
     host: "localhost",
   },
   plugins: [
+    // mpn install html-webpack-plugin 安装
+    new HtmlWebpackPlugin({
+        // 以 public/index.html 为模板创建文件
+        // 新的html文件有两个特点：1. 内容和源文件一致 2. 自动引入打包生成的js等资源
+        template: path.resolve(__dirname, "./src/index.html"),
+    }),
+
     // Add your plugins here
     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
